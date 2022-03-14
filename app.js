a8cca046feec1fe2a82b93eaea8503496e5b6aa0
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-14 11:42:17 -0400

    Configure for Heroku

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
