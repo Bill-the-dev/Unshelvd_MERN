9bb933cbedce1bff34ea0d1916252318ee329076
kirbyneaton <kjneat@gmail.com>
2022-03-15 11:57:35 -0400

    Complete games action, util, and reducer

---
+++ frontend/src/reducers/games_reducer.js
@@ -0,0 +1,21 @@
+import {
+  RECEIVE_GAMES,
+  RECEIVE_GAME,
+} from '../actions/game_actions';
+
+const GamesReducer = (state = { games: {}, game: {}}, action) => {
+  Object.freeze(state);
+  let nextState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_GAMES:
+      nextState.games = action.games.data
+      return nextState;
+    case RECEIVE_GAME:
+      nextState.game = action.game.data;
+      return nextState;
+    default:
+      return state;
+  }
+};
+
+export default GamesReducer;
