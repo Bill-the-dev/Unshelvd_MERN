7e7d74363e6b0a982bb3b3be2242a7d5b4deef10
kirbyneaton <kjneat@gmail.com>
2022-03-16 09:42:36 -0400

    Old version of games state in game show and game show container

--- frontend/src/components/game/game_show.js
+++ frontend/src/components/game/game_show.js
@@ -40,16 +40,16 @@ class GameShow extends React.Component {
                     {/* <h1 id="game-show-title">{this.state.entities.games.game.title}</h1> */}
 
                     <h1 id="game-show-title">{game.name}</h1>
-                    {/* <img src="" alt="game" id="game-photo" />
-                    <h2 id="game-show-status">{currentGame.status}</h2>
-                    <h2 id="game-show-num-players">{currentGame.numPlayers}</h2>
-                    <h2 id="game-show-category">{currentGame.category}</h2>
-                    <p id="game-show-description">{currentGame.description}</p>
-                    <a id="game-show-rules" href="">{currentGame.rules}</a>
-                    <h2 id="game-show-setting">{currentGame.setting}</h2> */}
+                    <img src="" alt="game" id="game-photo" />
+                    <h2 id="game-show-status">{game.status}</h2>
+                    <h2 id="game-show-num-players">{game.numPlayers}</h2>
+                    <h2 id="game-show-category">{game.category}</h2>
+                    <p id="game-show-description">{game.description}</p>
+                    <a id="game-show-rules" href="">{game.rules}</a>
+                    <h2 id="game-show-setting">{game.setting}</h2>
 
-                    {/* <Link to={`/game/${currentGame.id}/edit`}><button onClick={this.props.closeModal} id="edit-game">Edit Game</button></Link>
-                    <button onClick={() => this.props.deleteGame(currentGame.id)} id="delete-game">Delete Game</button> */}
+                    {/* <Link to={`/game/${game.id}/edit`}><button onClick={this.props.closeModal} id="edit-game">Edit Game</button></Link>
+                    <button onClick={() => this.props.deleteGame(game.id)} id="delete-game">Delete Game</button> */}
                 </div>
             </div>
         );
