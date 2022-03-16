8ff35ed071ae90dcceea33ece4393763040c9f87
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-15 20:05:22 -0400

    .

--- frontend/src/reducers/games_reducer.js
+++ frontend/src/reducers/games_reducer.js
@@ -8,6 +8,7 @@ const GamesReducer = (state = { games: {}, game: {}}, action) => {
   let nextState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_GAMES:
+      debugger
       nextState.games = action.games.data
       return nextState;
     case RECEIVE_GAME:
