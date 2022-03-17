be45e7cd9dd9c06b71c1f608abd35860280ebeb1
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-17 19:17:30 -0400

    Remove gitignore for keys

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     mongoURI: 'mongodb+srv://unshelvd_admin:YifZ39iKBWmh3g1m@unshelvd.ek5yq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+//     secretOrKey: 'secret'
+//   }
+
+  if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+  } else {
+    module.exports = require('./keys_dev');
+  }
