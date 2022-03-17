398d024e3023e658acd2142d902456936228ea6d
bill-the-dev <billthedev@gmail.com>
2022-03-17 09:38:33 -0400

    Add seed-file asynch logic, load order was incorrect

--- seed_data_games.js
+++ seed_data_games.js
@@ -1,135 +1,142 @@
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const { schema } = require('./models/Game');
 const User = require('./models/User');
 const Group = require('./models/Group');
 const Game = require('./models/Game');
-const { schema } = require('./models/Game');
 
-// mongoose
-//   .connect('mongodb://localhost/   ', {
-//     useNewUrlParser: true, useUnifiedTopology: true 
-//   }).then(() => {
-//     console.log('Mongo connection open');
-//   });
 
-const seedUsers = [
-  {
-    username: '',
-    email: '',
-    password: '',
-    games: [{
-      // type: Schema.Types.ObjectId,
-      // ref: 'Game'
-    }],
-    groups: [{
-      // Schema.Types.ObjectId,
-      // ref: 'Group'
-    }],
-  },
-] 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => loadCounter())
+  .catch(err => console.log(err));
 
-const seedGames = [
-  {
-    name: '',
-    image: '',
-    playerCount: {
-      min: 0,
-      max: 0
-    },
-    category: [
+// const seedUsers = [
+//   {
+//     username: '',
+//     email: '',
+//     password: '',
+//     games: [{
+//       // type: Schema.Types.ObjectId,
+//       // ref: 'Game'
+//     }],
+//     groups: [{
+//       // Schema.Types.ObjectId,
+//       // ref: 'Group'
+//     }],
+//   },
+// ] 
 
-    ],
-    gameType: [
+// const seedGames = [
+//   {
+//     name: '',
+//     image: '',
+//     playerCount: {
+//       min: 0,
+//       max: 0
+//     },
+//     category: [
 
-    ],
-    description: '',
-    userCreator: ''
-  }
-]
+//     ],
+//     gameType: [
 
-const seedGroups = [
-  {
-    name: '',
-    users: [
+//     ],
+//     description: '',
+//     userCreator: ''
+//   }
+// ]
 
-    ],
-    userGroupCreator: {
-      // type: Schema.Types.ObjectId,
-      // ref: 'User'
-    }
-  }
-]
+// const seedGroups = [
+//   {
+//     name: '',
+//     users: [
 
-const games = [
+//     ],
+//     userGroupCreator: {
+//       // type: Schema.Types.ObjectId,
+//       // ref: 'User'
+//     }
+//   }
+// ]
+
+const gamesTest = [
   new Game(
     {
-      name: '',
-      image: '',
+      name: 'Codenames',
+      image: 'temp',
       playerCount: {
-        min: 0,
-        max: 0
+        min: 2,
+        max: 8
       },
       category: [
 
       ],
       gameType: [
-
+        'Board Game', 'Party', 'Word', 'Puzzle', 'Quick', 'Team Play', 'Deduction'
       ],
-      description: '',
-      userCreator: ''
-    }
-  ),
-  new Game(
-    {
-      name: '',
-      image: '',
-      playerCount: {
-        min: 0,
-        max: 0
-      },
-      category: [
-
-      ],
-      gameType: [
-
-      ],
-      description: '',
-      userCreator: ''
+      description: 'Codenames is an easy deduction party game. The game is divided into red and blue, each side has a team leader, the team leader\'s goal is to lead their team to the final victory.The two rival spymasters know the secret identities of 25 agents.Their teammates know the agents only by their CODENAMES. Connected Play: https://codenames.game/ Instructions: https://czechgames.com/en/codenames/video/',
+      userCreator: 'Unshelvd'
     }
   ),
   new Game(
     {
-      name: '',
-      image: '',
+      name: 'Scrabble',
+      image: 'temp',
       playerCount: {
-        min: 0,
-        max: 0
+        min: 2,
+        max: 4
       },
       category: [
-
+        'Board Game', 'Word', 'Tile'
       ],
       gameType: [
-
+        'Connected', 'Unplugged'
       ],
-      description: '',
-      userCreator: ''
+      description: 'Scrabble is a word game in which two to four players score points by placing tiles, each bearing a single letter, onto a game board divided into a 15×15 grid of squares. The tiles must form words that, in crossword fashion, read left to right in rows or downward in columns, and be included in a standard dictionary or lexicon. Connected Play: https://www.lexulous.com/',
+      userCreator: 'Unshelvd'
+    }
+  ),
+  new Game(
+    {
+      name: 'Spoons',
+      image: 'temp',
+      playerCount: {
+        min: 3,
+        max: 1000
+      },
+      category: [
+        'Playing Cards', 'Party', 'Quick'
+      ],
+      gameType: [
+        'Unplugged'
+      ],
+      description: 'Dig into this fast paced card swapping game!',
+      userCreator: 'Unshelvd'
     },
   )
 ]
 
-
-let gamesDone = 0;
-for (let i = 0; i < games.length; i++) {
-  games[i].save((err, res) => {
-    gamesDone++
-    if (gamesDone === games.length) {
-      exit() 
-    }
-  });
+const loadCounter = () => {
+  let gamesDone = 0;
+  for (let i = 0; i < gamesTest.length; i++) {
+    debugger
+    gamesTest[i].save((err, res) => {
+      gamesDone++
+      if (gamesDone === gamesTest.length) {
+        debugger
+        console.log(`${gamesTest[i]}`)
+        exit() 
+      }
+      debugger
+    });
+}
   // should add documents, then disconnect once finished (callback below)
   
 }
 const exit = () => {
-  mongoose.disconnect();
+  mongoose.disconnect()
+    .then(() => console.log('Disonnected from MongoDB successfully'))
 }
 
 
