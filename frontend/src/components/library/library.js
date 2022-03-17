3d9c2ce412fec62170e715921a1862446b4bf84f
kirbyneaton <kjneat@gmail.com>
2022-03-17 13:44:18 -0400

    Add user-specific game index

--- frontend/src/components/library/library.js
+++ frontend/src/components/library/library.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LibraryItem from './library_item';
+import { Link} from 'react-router-dom'
 
 
 
@@ -7,34 +8,50 @@ class Library extends React.Component {
   constructor(props) {
     super(props);
 
-
+    this.state = {
+      currentUserGames: [],
+      gameObjects: []
+    }
   }
 
   componentDidMount() {
     this.props.fetchGames();
     this.props.fetchUser(this.props.sessionUser.id)
-      .then(() => console.log(this.props))
-      // .then((currentUser) => setState(currentUser.games));
+    .then(() => console.log(this.props))
+    .then(() => this.setState({currentUserGames: this.props.userGames}))
+      .then(() => console.log(this.state))
+      .then(() => {
+        let games = []
+        this.props.allGames?.map(game => {
+          // debugger
+          if (this.state.currentUserGames.includes(game._id)){
+            // debugger
+            games.push(game)
+          }
+        })
+      this.setState({gameObjects: games})
+      console.log(games)
+      })
 
-    // this.props.fetchGame(this.props.match.params.id);
-    // this.props.fetchUserLibrary();
+    // this.props.fetchUserLibrary(this.props.sessionUser.id);
 
   }  
     
     render() {
         const {currentUser, userGames, fetchGames, fetchGame, createGame, fetchUserGames, openModal} = this.props
-        if (!userGames) return null;
+        // if (!userGames) return null;
         if (!currentUser) return null;
         debugger
         return(
           
           <div className='library-container'>
             <h1 className="welcome-msg--library">{currentUser.username}'s Library</h1>
-            <button className='button--add-game'>+Add Game</button>
+            
+            <Link to="/new/game"><button className='button--add-game'>+Add Game</button></Link>
             <div className='library-index-container'>
               <ul className='library-index-list'>
                 {
-                  userGames.map(game => <LibraryItem currentUser={currentUser} openModal={ openModal } game={ game }  key={ game._id } />)
+                  this.state.gameObjects.map(game =>  <LibraryItem currentUser={currentUser} openModal={ openModal } game={ game }  key={ game._id } />)
                 }
               </ul>
             </div>
--- frontend/src/components/library/library_container.js
+++ frontend/src/components/library/library_container.js
@@ -5,7 +5,8 @@ import Library from './library';
 
 const mapStateToProps = state => ({
     sessionUser: state.session.user, 
-    userGames: state.users.currentUser.games, 
+    currentUser: state.entities.users.currentUser, 
+    userGames: state.entities.users.currentUser.games, 
     //Current user's games will be rendered in the library
     allGames: Object.values(state.entities.games.userGames) //So that clicking on a game in library shows the game information (renders a game show/modal)
 })
