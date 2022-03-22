06f10949b84aa49c8ddff7e364d98e5450d29757
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-22 15:01:04 -0400

    Reformat team page

--- frontend/src/components/splash/team.jsx
+++ frontend/src/components/splash/team.jsx
@@ -5,12 +5,16 @@ import {faLinkedin} from '@fortawesome/fontawesome-free-brands'
 import { Link } from "react-router-dom";
 
 function TeamShow() {
-  // const bill = 
+  const emily = 'https://raw.githubusercontent.com/Bill-the-dev/Unshelvd_MERN/splash-format/frontend/src/components/splash/emily.jpg'
+  const bill = 'https://raw.githubusercontent.com/Bill-the-dev/Unshelvd_MERN/splash-format/frontend/src/components/splash/bill.jpg'
+  const ethan = 'https://raw.githubusercontent.com/Bill-the-dev/Unshelvd_MERN/splash-format/frontend/src/components/splash/ethan.jpg'
+  const kirby = 'https://raw.githubusercontent.com/Bill-the-dev/Unshelvd_MERN/splash-format/frontend/src/components/splash/kirby.jpg'
   return (
     <div className="team-container">
       <h1>Meet the team behind Unshelvd</h1>
       <div className="team-body">
         <div className="team-bill">
+          <img src={bill} className='team-photo'/>
           <p>Bill Camarco</p>
           <button>
             <a href="https://github.com/Bill-the-dev" target='_blank'>
@@ -24,7 +28,7 @@ function TeamShow() {
           </button>
         </div>
         <div className="team-emily">
-          <img src="./emily.jpg"/>
+          <img src={emily} className='team-photo'/>
           <p>Emily Lichtenberg</p>
           <button>
             <a href="https://github.com/emilylichtenberg" target='_blank'>
@@ -38,6 +42,7 @@ function TeamShow() {
           </button>
         </div>
         <div className="team-ethan">
+        <img src={ethan} className='team-photo'/>
           <p>Ethan Lam</p>
           <button>
             <a href="https://github.com/EthanLam17" target='_blank'>
@@ -51,6 +56,7 @@ function TeamShow() {
           </button>
         </div>
         <div className="team-kirby">
+        <img src={kirby} className='team-photo'/>
           <p>Kirby Neaton</p>
           <button>
             <a href="https://github.com/kirbyneaton" target='_blank'>
