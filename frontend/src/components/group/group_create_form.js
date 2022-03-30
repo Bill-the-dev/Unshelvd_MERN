21ca8e3fa64bd57d70ab994d8332b4253125a54f
Ethan Lam <ethanlam0@yahoo.com>
2022-03-30 09:10:00 -0400

    Update conditional to reflect updated changes

--- frontend/src/components/group/group_create_form.js
+++ frontend/src/components/group/group_create_form.js
@@ -51,22 +51,31 @@ class CreateGroup extends React.Component {
             errors: "Group does not exist"
           })
         } else {
-            // ADD GROUP TO USER
-          const updatedUser = this.props.currentUser;
+          const updatedUser = {...this.props.currentUser};
+          // debugger
           if (!updatedUser.groups.includes(curGroup._id)){
             // debugger
-              updatedUser.groups = this.props.currentUser.groups.concat(curGroup);
-              this.props.updateUser(updatedUser);
-      
+              updatedUser.groups = updatedUser.groups.concat(curGroup._id);
+              // debugger
+              
               // ADD USER TO GROUP
-              const updatedGroup = curGroup;
+              const updatedGroup = {...curGroup};
+              // debugger
               updatedGroup.users = curGroup.users.concat(this.props.currentUser._id);
               // debugger
+              this.props.updateUser(updatedUser);
               this.props.updateGroup(updatedGroup)
-              .then(() => this.props.fetchGroups())
+                .then(() => {
+                  // debugger
+                  this.props.closeModal()
+                  this.props.history.push({pathname: `/groups/${curGroup._id}`})
+                })
+              // .then(group => this.props.fetchGroup(group._id))
+              // .then(() => this.props.fetchGroups())
+            } else {
+              this.props.closeModal()
+              this.props.history.push({pathname: `/groups/${curGroup._id}`})
             }
-            this.props.closeModal()
-            setTimeout(this.props.history.push({pathname: `/groups/${curGroup._id}`}),1000)
         }
     }
   }
